0206f689ac21edaf9b2da37c9bc26abf169550b2
VCKazakova <v.c.kazakova@gmail.com>
2023-09-15 12:39:02 +0300

    Add methods to NoteRepository

--- src/main/java/ru/vckazakova/notebot/repository/NoteRepository.java
+++ src/main/java/ru/vckazakova/notebot/repository/NoteRepository.java
@@ -1,7 +1,24 @@
 package ru.vckazakova.notebot.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import ru.vckazakova.notebot.model.Note;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface NoteRepository extends MongoRepository<Note, String> {
+
+    List<Note> findAllByTag(String tag);
+
+    @Query("{$expr: {$eq: [{$year: '$dateTime'}, ?0]}}")
+    List<Note> findAllByYear(int year);
+
+    @Query("{$and: [{$expr: {$eq: [{$year: '$dateTime'}, ?0]}}, {'tag': ?1}]}")
+    List<Note> findAllByYearAndTag(int year, String tag);
+
+    List<Note> findAllByDateTimeBetween(LocalDateTime fromDate, LocalDateTime toDate);
+
+    List<Note> findAllByDateTimeBetweenAndTag(LocalDateTime fromDate, LocalDateTime toDate, String tag);
+
 }
