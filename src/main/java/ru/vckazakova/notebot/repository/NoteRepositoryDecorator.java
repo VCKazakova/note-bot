0b814c84df2409ccf38d9f1bbc86a70a5e550c81
VCKazakova <v.c.kazakova@gmail.com>
2023-09-15 15:41:36 +0300

    Add realizations ant tests for NoteRepositoryDecoratorImpl

--- src/main/java/ru/vckazakova/notebot/repository/NoteRepositoryDecorator.java
+++ src/main/java/ru/vckazakova/notebot/repository/NoteRepositoryDecorator.java
@@ -2,9 +2,12 @@ package ru.vckazakova.notebot.repository;
 
 import ru.vckazakova.notebot.model.Note;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NoteRepositoryDecorator {
+
+    void saveNote(Note note);
 
     List<Note> findNotesForCurrentDay();
 
@@ -17,5 +20,17 @@ public interface NoteRepositoryDecorator {
     List<Note> findNotesForCurrentMonth();
 
     List<Note> findNotesForCurrentMonthByTag(String tag);
+
+    List<Note> findAllNotes();
+
+    List<Note> findAllNotesByPeriod(LocalDateTime fromDate, LocalDateTime toDate);
+
+    List<Note> findAllNotesByPeriodAndTag(LocalDateTime fromDate, LocalDateTime toDate, String tag);
+
+    List<Note> findAllNotesByTag(String tag);
+
+    List<Note> findAllNotesByYear(int year);
+
+    List<Note> findAllNotesByYearAndTag(int year, String tag);
 
 }
--- src/test/java/ru/vckazakova/notebot/repository/NoteRepositoryDecoratorImplTest.java
+++ src/test/java/ru/vckazakova/notebot/repository/NoteRepositoryDecoratorImplTest.java
@@ -1,5 +1,6 @@
 package ru.vckazakova.notebot.repository;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,9 @@ import ru.vckazakova.notebot.model.Note;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @DisplayName("NoteRepositoryDecoratorImpl should:")
@@ -24,12 +26,30 @@ class NoteRepositoryDecoratorImplTest extends IntegrationBased {
     @Autowired
     private NoteRepositoryDecorator noteRepositoryDecorator;
 
+    private final LocalDateTime startDate = LocalDateTime.of(2023, Month.AUGUST, 1, 0, 0);
+    private final LocalDateTime endDate = LocalDateTime.of(2023, Month.AUGUST, 31, 23, 59);
+
     @BeforeEach
     public void init() {
         Note note = new Note("1", "#здоровье", "чтобы быть здоровым, пей витамины", LocalDateTime.now());
         Note note2 = new Note("2", "#films", "The best film", LocalDateTime.of(2023, Month.AUGUST, 10, 12, 3));
         noteRepository.save(note);
         noteRepository.save(note2);
+    }
+
+    @AfterEach
+    public void flush() {
+        noteRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("сохранять заметку")
+    public void saveNoteTest() {
+        Note note = new Note("3", "#java", "OOP - the most important principles", LocalDateTime.now());
+        noteRepositoryDecorator.saveNote(note);
+        Optional<Note> byId = noteRepository.findById("3");
+        assertTrue(byId.isPresent());
+        assertEquals("#java", byId.get().getTag());
     }
 
     @Test
@@ -73,6 +93,52 @@ class NoteRepositoryDecoratorImplTest extends IntegrationBased {
     public void findNotesForCurrentMonthByTagTest() {
         List<Note> notesForCurrentMonth = noteRepositoryDecorator.findNotesForCurrentMonthByTag("#здоровье");
         assertEquals(1, notesForCurrentMonth.size());
+    }
+
+    @Test
+    @DisplayName("найти все заметки")
+    public void findAllNotesTest() {
+        List<Note> allNotes = noteRepositoryDecorator.findAllNotes();
+        assertEquals(2, allNotes.size());
+    }
+
+    @Test
+    @DisplayName("найти все заметки за период")
+    public void findAllNotesByPeriodTest() {
+        List<Note> allNotesByPeriod =
+                noteRepositoryDecorator.findAllNotesByPeriod(startDate,
+                        endDate);
+        assertEquals(1, allNotesByPeriod.size());
+    }
+
+    @Test
+    @DisplayName("найти все заметки за период по тэгу")
+    public void findAllNotesByPeriodAndTagTest() {
+        List<Note> allNotesByPeriodAndTag =
+                noteRepositoryDecorator.findAllNotesByPeriodAndTag(startDate, endDate, "#здоровье");
+        assertTrue(allNotesByPeriodAndTag.isEmpty());
+    }
+
+    @Test
+    @DisplayName("найти все заметки по тэгу")
+    public void findAllNotesByTagTest() {
+        List<Note> notesByTag = noteRepositoryDecorator.findAllNotesByTag("#films");
+        assertEquals(1, notesByTag.size());
+    }
+
+    @Test
+    @DisplayName("найти все заметки по году")
+    public void findAllNotesByYearTest() {
+        List<Note> allNotesByYear = noteRepositoryDecorator.findAllNotesByYear(2023);
+        assertFalse(allNotesByYear.isEmpty());
+    }
+
+    @Test
+    @DisplayName("найти все заметки по году и тэгу")
+    public void findAllNotesByYearAndTagTest() {
+        List<Note> notesByYearAndTag = noteRepositoryDecorator.findAllNotesByYearAndTag(2023, "#films");
+        List<Note> all = noteRepository.findAll();
+        assertEquals(1, notesByYearAndTag.size());
     }
 
 }
