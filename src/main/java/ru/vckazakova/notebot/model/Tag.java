58a615be37baac6901ad30b271745f12891f186e
VCKazakova <v.c.kazakova@gmail.com>
2023-09-18 16:43:01 +0300

    Add explicit imports

--- src/main/java/ru/vckazakova/notebot/model/Tag.java
+++ src/main/java/ru/vckazakova/notebot/model/Tag.java
@@ -1,6 +1,10 @@
 package ru.vckazakova.notebot.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
